dc34c8b82e63e4830cf2663b0c0c944b55537936
Jamerson Felix <jamersonbruno91@gmail.com>
2025-04-06 23:50:45 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'joca-app';
 }
+@NgModule({
+    imports: [
+    ReactiveFormsModule,
+    // outros módulos
+  ]
+})
+export class AppModule { }
