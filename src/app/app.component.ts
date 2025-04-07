d05ebdab3ba25e165fd0ffb88cff56ab728f911b
Jamerson Felix <jamersonbruno91@gmail.com>
2025-04-08 00:07:53 +0100

    feat: add o formulario e as validações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RegisterComponent } from './components/register/register.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, RegisterComponent],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from "../login/login.component";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, LoginComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  mostrarRequisitosSenha = false;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      nome: ['', Validators.required],
+      sobrenome: ['', Validators.required],
+      telefone: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      senha: ['', Validators.required, Validators.minLength(10)],
+      confirmarSenha: ['', Validators.required, Validators.minLength(10)],
+      termos: [false, Validators.requiredTrue]
+    }, {Validators: this.matchPasswords });
+  }
+
+  matchPasswords(group: FormGroup) {
+    const senha = group.get('senha')?.value;
+    const confirmarSenha = group.get('confirmarSenha')?.value;
+    return senha === confirmarSenha ? null : {mismatch: true};
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Dados de cadastro:', this.registerForm.value);
+      // Enviar dados ao backend no futuro
+    }
+  }
+
+  toggleRequisitosSenha(){
+    this.mostrarRequisitosSenha = true;
+  }
 
 }
