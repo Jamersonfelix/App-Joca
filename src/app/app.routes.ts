f54a35c2640fdc4f8a83e571e9d534e86083de76
Jamerson Felix <jamersonbruno91@gmail.com>
2025-04-10 23:54:27 +0100

    feat: conclusão da page register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
 ];
 
