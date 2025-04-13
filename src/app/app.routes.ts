1224b0d85020796a3fc8414d5690bfbe21b6f58d
Jamerson Felix <jamersonbruno91@gmail.com>
2025-04-13 22:16:24 +0100

    ajuste app.router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { ConfirmationAccontComponent } from './components/confirmation-accont/confirmation-accont.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full'},
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'forgot-password' , component: ForgotPasswordComponent},
-    { path: 'reset-password', component: ResetPasswordComponent}
+    { path: 'reset-password', component: ResetPasswordComponent},
+    { path: 'confirmation-accont', component: ConfirmationAccontComponent},
 ];
 
 @NgModule ({
