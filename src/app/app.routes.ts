d0fc6fca157b2abfa35112060ed5e3a05aafd2ac
Jamerson Felix <jamersonbruno91@gmail.com>
2025-04-13 19:53:34 +0100

    finalizando a logica "forgot-password" e criado o component "reset-password"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full'},
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'forgot-password' , component: ForgotPasswordComponent},
+    { path: 'reset-password', component: ResetPasswordComponent}
 ];
 
 @NgModule ({
--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
+  forgotForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.forgotForm = this.fb.group({
+      email: ['', Validators.required, Validators.email]
+    });
+  }
+
+  onSubmit() {
+    if (this.forgotForm.valid) {
+      console.log('Solicitação enviada:', this.forgotForm.value.email);
+      this.router.navigate(['/reset-password']);
+    }
+  }
 }
